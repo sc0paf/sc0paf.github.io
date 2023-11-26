a2a10337c2227b72063747ef75756b5f67871b14
sc0paf <tm.domeika@gmail.com>
2023-11-25 21:13:33 -0500

    more

--- main.js
+++ main.js
@@ -3,17 +3,36 @@ const cardBody = document.getElementById('cardBody')
 const cardPopUp = document.getElementById('cardDisplay')
 const moneySpan = document.getElementById('moneySpan')
 
+// find sq by name     const square = player.squares[which].find(obj => obj.id === id)
+
 // game data
 const buildings = {
     Generator: {
-        name: 'Mint',
+        name: 'Generator',
         description: 'Generates $1.',
-        cost: 5
+        cost: 5,
+        upgrades: {
+            output: {
+                id: 'Output',
+                description: 'Doubles the amount generated',
+                cost: 25
+            },
+            crit: {
+                id: 'Crit',
+                description: 'Adds a small chance to gain 3x.',
+                cost: 25
+            }
+        }
     },
     Multi: {
         name: 'Multi',
         description: 'Multiplies the next square.',
         cost: 10
+    },
+    Reverse: {
+        name: 'Reverse',
+        description: 'Reverses direction when hitting this square.',
+        cost: 25
     }
 }
 
@@ -22,7 +41,8 @@ const player = {
     iterationSpeed: 1000,
     activeSquares: ['a'],
     squares: {
-        a: []
+        a: [],
+        b: []
     }
 }
 
@@ -42,9 +62,11 @@ squaresA[0].style.backgroundColor = 'black'
 squaresA[0].style.color = 'white'
 
 let iterator = 1
+let iteratorB = 1
+let iteratorC = 1
 
 function init() {
-    moneySpan.innerHTML = player.money
+    moneySpan.innerHTML = `$${player.money}`
     if (player.activeSquares.includes('a')) {
         // Draw the board for A
         squaresA.forEach((element) => {
@@ -60,25 +82,82 @@ function init() {
                 type: 'None'
             }
             player.squares.a.push(newPlayerSquare)
+        })        
+    }
+    if(player.activeSquares.includes('b')) {
+        squaresB.forEach((element) => {
+            let newSpan = document.createElement('span')
+            newSpan.appendChild(document.createTextNode(`[ ${element.id} ]`))
+            newSpan.classList.add('squareSel')
+            newSpan.addEventListener('click', () => {
+                selectSquare('b', element.id)
+            })
+            element.appendChild(newSpan)
+            let newPlayerSquare = {
+                id: element.id,
+                type: 'None'
+            }
+            player.squares.b.push(newPlayerSquare)
         })
-        
     }
 }
 
 init()
 
 function buyBuilding(key, square) {
-    console.log(`${key} and ${square}`)
+    //key is Generator and square is a0
+    //console.log(`key is ${key} and square is ${square}`)
+    if (player.money < buildings[key].cost) return
+    let row = square.charAt(0)
+    let indexOfSquare = player.squares[row].findIndex(squares => squares.id === square)
+    console.log(player.squares[row][indexOfSquare].type)
+    if (player.squares[row][indexOfSquare].type !== 'None') return
+
+    if (key === 'Generator') {
+        let squareDiv = document.getElementById(square)
+        let newDiv = document.createElement('div')
+        newDiv.appendChild(document.createTextNode('G'))
+        squareDiv.appendChild(newDiv)
+        player.money -= buildings.Generator.cost
+        player.squares.a[indexOfSquare] = {
+            id: square,
+            type: 'Generator',
+            amount: 2
+        }       
+    }
+    // redraw the card IG
+    drawSquareBody(row, square)
 }
 
+
 function selectSquare(which, id) {
+    cardPopUp.style.display = 'block'
+    cardPopUp.style.top = (event.pageY + cardPopUp.clientHeight / 2) + 'px';
+    cardPopUp.style.left = (event.pageX + cardPopUp.clientWidth / 2) + 'px';
+    //console.log(`which is ${which} and id is ${id}`)
+    drawSquareBody(which, id)
+}
+
+function drawSquareBody(which, id) {
     cardHeader.innerHTML = ''
     cardBody.innerHTML = ''
     // select the square in the player object
     const square = player.squares[which].find(obj => obj.id === id)
     cardHeader.innerHTML = id
-    if (square.id === 'Generator') {
+    if (square.type === 'Generator') {
         //TODO: GENERATOR LOGIC
+        let topBody = document.createElement('strong')
+        topBody.appendChild(document.createTextNode('Generator'))
+        cardBody.appendChild(topBody)
+        cardBody.appendChild(document.createElement('br'))
+        let headText = document.createElement('span')
+        headText.appendChild(document.createTextNode('Upgrades'))
+        headText.style.textDecoration = 'underline'
+        cardBody.appendChild(headText)
+
+        for (const key in buildings.Generator.upgrades) {
+            
+        }
         
     } else {
         let topBody = document.createElement('span')
@@ -103,16 +182,28 @@ function selectSquare(which, id) {
             cardBody.appendChild(buildingButton)
         }
     }
-    cardPopUp.style.display = 'block'
 }
 
 function closeSquare() {
-
     cardPopUp.style.display = 'none'    
 }
 
 const theLoop = setInterval(() => {
-    let currentSelection = squaresA[iterator];
+    let currentSelectionA = squaresA[iterator];
+    if (player.activeSquares.includes('b')) {
+        currentSelectionB = squaresB[iteratorB];
+
+        let lastSelectionB = squaresB[(iteratorB - 1 + squaresB.length) % squaresB.length]
+        if (lastSelectionB) {
+            lastSelectionB.style.backgroundColor = 'white'
+            lastSelectionB.style.color = 'black'
+        }
+
+        currentSelectionB.style.backgroundColor = 'black'
+        currentSelectionB.style.color = 'white'
+        iteratorB = (iteratorB + 1) % squaresB.length
+                
+    }
     let lastSelection = squaresA[(iterator - 1 + squaresA.length) % squaresA.length];
     
     // Reset styles for the last selection
@@ -122,8 +213,12 @@ const theLoop = setInterval(() => {
     }
 
     // Apply styles for the current selection
-    currentSelection.style.backgroundColor = 'black';
-    currentSelection.style.color = 'white';
+    currentSelectionA.style.backgroundColor = 'black';
+    currentSelectionA.style.color = 'white';
+    if(player.squares.a[iterator].type === 'Generator') {       
+        player.money += player.squares.a[iterator].amount
+    }
+    moneySpan.innerHTML = `$${player.money}`
 
     iterator = (iterator + 1) % squaresA.length;
     
